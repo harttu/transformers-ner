7084299b0a7443682bf8ab63a5c3ec6e8971c492
Harttu Toivonen <harttu.toivonen@gmail.com>
2020-10-05 14:05:04 +0300

    s800 added

--- slurm-run.sh
+++ slurm-run.sh
@@ -16,19 +16,22 @@ ln -s $SLURM_JOBID.err logs/latest.err
 
 module load gcc/8.3.0 cuda/10.1.168
 
+
+source venv/bin/activate
 #python3 scripts/preprocess.py data/dev.txt.tmp $BERT_MODEL $MAX_LENGTH  > data/dev.txt
+echo "dollar1 is $1"
 
 MAX_LENGTH=128
 BERT_MODEL=bert-base-multilingual-cased
 
-OUTPUT_DIR=germeval-model
+OUTPUT_DIR=s800_1
 BATCH_SIZE=32
 NUM_EPOCHS=3
 SAVE_STEPS=750
 SEED=1
 
 python3 run_tf_ner.py --data_dir ./data/ \
---labels ./data/labels.txt \
+--labels ./s800/conll/labels.txt \
 --model_name_or_path $BERT_MODEL \
 --output_dir $OUTPUT_DIR \
 --max_seq_length  $MAX_LENGTH \
